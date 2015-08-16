8518fa988f74ac14b355e51605657a1f6b0b2d75
bgreer5050 <bgreer5050@gmail.com>
2015-08-16 17:28:08 -0500

    Made SendEmail Async, NOT TESTED

---
+++ Utilities/SparkEmail.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkPi.Utilities
+{
+    public class SparkEmail
+    {
+
+        public static async void Send(string strBody)
+        {
+
+
+            var pairs = new List<KeyValuePair<string, string>>
+                 {
+                    new KeyValuePair<string, string>("", "abc")
+                 };
+
+            var content = new FormUrlEncodedContent(pairs);
+
+            var client = new HttpClient { BaseAddress = new Uri("http://10.0.0.1") };
+            //  var response = client.PostAsync("/Messaging/SendEmail", content).Result;
+            Task<HttpResponseMessage> getStringTask = client.PostAsync("/Messaging/SendEmail", content);
+            HttpResponseMessage message = await client.PostAsync("/Messaging/SendEmail", content);
+            
+        }
+    }
+}
