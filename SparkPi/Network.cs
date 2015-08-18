c247277806844f464cf21edf92ac368ead4f9555
bgreer5050 <bgreer5050@gmail.com>
2015-08-18 08:51:29 -0500

    Commented out MAC Address get until I find a fix.

--- SparkPi/Network.cs
+++ SparkPi/Network.cs
@@ -19,39 +19,53 @@ namespace SparkPi
         {
             get
             {
-                if (this._macAddress == null || this._macAddress == "")
-                {
-                    
-                    NetworkAdapter nics = NetworkInterface.GetAllNetworkInterfaces();
-                    if (nics == null || nics.Length < 1)
-                    {
-                        Debug.Print("  No network interfaces found.");
-                        return "";
-                    }
-                    else
-                    {
-                        IPInterfaceProperties properties = nics[0].GetIPProperties();
-                        PhysicalAddress address = nics[0].GetPhysicalAddress();
-                        byte[] bytes = address.GetAddressBytes();
-                        for (int i = 0; i < bytes.Length; i++)
-                        {
-                            // Display the physical address in hexadecimal.
-                            Debug.Print("{0}", bytes[i].ToString("X2"));
-                            // Insert a hyphen after each byte, unless we are at the end of the  
-                            // address. 
-                            if (i != bytes.Length - 1)
-                            {
-                                Debug.Print("-");
-                            }
-                        }
-                    }
-                }
-                return _macAddress;
+                return "NO MAC ADDRESS FOUND";
             }
             set
             {
                 _macAddress = value;
             }
         }
+
+        //public string MacAddress
+        //{
+        //    get
+        //    {
+        //        if (this._macAddress == null || this._macAddress == "")
+        //        {
+                    
+        //            NetworkAdapter nics = NetworkInterface.GetAllNetworkInterfaces();
+        //            if (nics == null || nics.Length < 1)
+        //            {
+        //                Debug.Print("  No network interfaces found.");
+        //                return "";
+        //            }
+        //            else
+        //            {
+        //                IPInterfaceProperties properties = nics[0].GetIPProperties();
+        //                PhysicalAddress address = nics[0].GetPhysicalAddress();
+        //                byte[] bytes = address.GetAddressBytes();
+        //                for (int i = 0; i < bytes.Length; i++)
+        //                {
+        //                    // Display the physical address in hexadecimal.
+        //                    Debug.Print("{0}", bytes[i].ToString("X2"));
+        //                    // Insert a hyphen after each byte, unless we are at the end of the  
+        //                    // address. 
+        //                    if (i != bytes.Length - 1)
+        //                    {
+        //                        Debug.Print("-");
+        //                    }
+        //                }
+        //            }
+        //        }
+        //        return _macAddress;
+        //    }
+        //    set
+        //    {
+        //        _macAddress = value;
+        //    }
+        //}
+
+
     }
 }
