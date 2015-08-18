5509060dfc016dd6f8e57dfd21404c3246d888a0
bgreer5050 <bgreer5050@gmail.com>
2015-08-18 13:16:53 -0500

    SparkEmail works

--- SparkPiMockUI/Utilities/SparkEmail.cs
+++ SparkPiMockUI/Utilities/SparkEmail.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,19 +16,46 @@ namespace SparkPiMockUI.Utilities
         public static async void Send(string strBody)
         {
 
+            try
+            {
+                string strEmailMessage = "to=bgreer@metal-matic.com&from=apollo@metal-matic.com&subject=Spark Issue&body=" + strBody;
 
-            var pairs = new List<KeyValuePair<string, string>>
-                 {
-                    new KeyValuePair<string, string>("", "abc")
-                 };
+                //create a request
+                var request = (HttpWebRequest)WebRequest.Create("http://10.0.100.16/Messaging/SendEmail");
 
-            var content = new FormUrlEncodedContent(pairs);
+                byte[] buffer = Encoding.UTF8.GetBytes(strEmailMessage);
 
-            var client = new HttpClient { BaseAddress = new Uri("http://10.0.0.1") };
-            //  var response = client.PostAsync("/Messaging/SendEmail", content).Result;
-            Task<HttpResponseMessage> getStringTask = client.PostAsync("/Messaging/SendEmail", content);
-            HttpResponseMessage message = await client.PostAsync("/Messaging/SendEmail", content);
 
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-urlencoded";
+
+
+                Task<Stream> postTask = request.GetRequestStreamAsync();
+
+                //We could do other stuff here since we have not called await on our task object yet.
+
+                Stream post = await postTask;
+                post.Write(buffer, 0, buffer.Length);
+                post.Flush();
+
+                Task<WebResponse> responseTask = request.GetResponseAsync();
+                WebResponse webResponse = await responseTask;
+
+                HttpWebResponse response = (HttpWebResponse)webResponse;
+                Stream responseStream = response.GetResponseStream();
+                Debug.Print(response.StatusCode.ToString());
+
+                StreamReader reader = new StreamReader(responseStream);
+                var responseValue = reader.ReadToEnd();
+
+
+            }
+            catch (Exception Ex)
+            {
+
+                Debug.Write(Ex.Message.ToString());
+
+            }
         }
     }
 }
