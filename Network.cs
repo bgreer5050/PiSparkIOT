02de69fd193a83054ab67c6a3971710d2fa63df6
bgreer5050 <bgreer5050@gmail.com>
2015-08-17 21:51:33 -0500

    Network class - get mac is working in Mock project, need to get working in main project.

--- Network.cs
+++ Network.cs
@@ -2,30 +2,56 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Networking.Connectivity;
 
 namespace SparkPi
 {
-    //public class Network
-    //{
-    //    private IPAddress _ipAddress;
+    public class Network
+    {
+        private string _macAddress;
+        private bool _networkUp;
+        public bool IsNetworkAvailable; //Use NetworkINterface.IsNetworkAvailabe
 
-    //    //public async Task<IPAddress> GetIPAddress()
-    //    //{
+        public string MacAddress
+        {
+            get
+            {
+                if (this._macAddress == null || this._macAddress == "")
+                {
 
-    //    //        Task<IPAddress> address = await NetworkInformation.GetConnectionProfiles().FirstOrDefault().NetworkAdapter.GetConnectedProfileAsync();
-    //    //        return address;
-    //    //    }
-    //    //    set
-    //    //    {
-    //    //        _ipAddress = value;
-
-    //    //    }
-    //    //}
-
-    
-
-    //}
+                    NetworkAdapter nics = NetworkInterface.GetAllNetworkInterfaces();
+                    if (nics == null || nics.Length < 1)
+                    {
+                        Debug.Print("  No network interfaces found.");
+                        return "";
+                    }
+                    else
+                    {
+                        IPInterfaceProperties properties = nics[0].GetIPProperties();
+                        PhysicalAddress address = nics[0].GetPhysicalAddress();
+                        byte[] bytes = address.GetAddressBytes();
+                        for (int i = 0; i < bytes.Length; i++)
+                        {
+                            // Display the physical address in hexadecimal.
+                            Debug.Print("{0}", bytes[i].ToString("X2"));
+                            // Insert a hyphen after each byte, unless we are at the end of the  
+                            // address. 
+                            if (i != bytes.Length - 1)
+                            {
+                                Debug.Print("-");
+                            }
+                        }
+                    }
+                }
+                return _macAddress;
+            }
+            set
+            {
+                _macAddress = value;
+            }
+        }
+    }
 }
