3f6e26be292d2f818c33a808e915885c0c52f304
bgreer5050 <bgreer5050@gmail.com>
2015-09-24 05:54:49 -0500

    155 Production

--- SparkPi/Controller.cs
+++ SparkPi/Controller.cs
@@ -57,7 +57,7 @@ namespace SparkPi
 
         public Configuration()
         {
-            this.AssetNumber = "9999";
+            this.AssetNumber = "155";
             this._gracePeriodMultiple = 2.0f;
             this.CycleLengthMs = 80000;
             this.HeartbeatsRequiredToChangeState = 2;
