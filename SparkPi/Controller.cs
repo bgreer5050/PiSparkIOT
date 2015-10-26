32a5c7c67c86056a988d6debf3be653828710a3d
bgreer5050 <bgreer5050@gmail.com>
2015-10-26 12:22:01 -0500

    Misc commit

--- SparkPi/Controller.cs
+++ SparkPi/Controller.cs
@@ -61,10 +61,10 @@ namespace SparkPi
 
         public Configuration()
         {
-            this.AssetNumber = "768";
+            this.AssetNumber = "9999";
             this._gracePeriodMultiple = 2.0f;
             this.CycleLengthMs = 40000;
-            this.HeartbeatsRequiredToChangeState = 1;
+            this.HeartbeatsRequiredToChangeState = 2;
 
             this.Errors = new List<string>();
             this.ObjectMessages = new List<string>();
